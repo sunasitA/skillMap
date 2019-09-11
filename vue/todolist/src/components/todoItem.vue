e400e9d5058d21e499874163c1d00fe07621a473
sunasitA <sunnana.fnst@cn.fujitsu.com>
2019-09-11 09:52:23 +0800

    提交vue项目相关内容

---
+++ vue/todolist/src/components/todoItem.vue
@@ -0,0 +1,21 @@
+<template>
+ <li @click="handleClick">
+    {{content}}
+ </li>
+</template>
+
+<script>
+//子组件用props接收父组件传递的参数
+export default {
+  props:  ['content', 'index'],
+  methods: {
+      handleClick: function(){
+          this.$emit('delete', this.index);
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
